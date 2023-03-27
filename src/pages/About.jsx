a101e422634fe77a15aa9e22f9084c185b6a61a9
HarshBansal2k01 <ihbbro14@gmail.com>
2023-03-27 10:04:46 +0530

    yo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import "../style/about.css";
 
 function About() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="about-section">
+        <h1>About Us Page</h1>
+        <p>Lorem ipsum dolor sit amet..</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae,
+          iusto!
+        </p>
+      </div>
+      <h2 style={{ textAlign: "center" }}>Our Team</h2>
+
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <img
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/35af6a41332353.57a1ce913e889.jpg"
+              alt="Mike"
+              style={{ width: "100%" }}
+            />
+            <div className="container">
+              <h2>Mike Ross</h2>
+              <p className="title">Art Director</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+                perspiciatis tempore voluptates deserunt distinctio itaque est
+                alias quis perferendis ipsa!.
+              </p>
+              <p>mike@gmail.com</p>
+              <p>
+                <button className="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="column">
+          <div className="card">
+            <img
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/35af6a41332353.57a1ce913e889.jpg"
+              alt="Mike"
+              style={{ width: "100%" }}
+            />
+            <div className="container">
+              <h2>Mike Ross</h2>
+              <p className="title">Art Director</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+                perspiciatis tempore voluptates deserunt distinctio itaque est
+                alias quis perferendis ipsa!.
+              </p>
+              <p>mike@gmail.com</p>
+              <p>
+                <button className="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import "../style/contact.css"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <div className="container">
+  <div style={{textAlign:"center"}}>
+    <h2>Contact Us</h2>
+    <p>Swing by for a cup of coffee, or leave us a message:</p>
+  </div>
+  <div className="row">
+    <div className="column">
+      <img src="https://static.vecteezy.com/system/resources/previews/004/283/797/original/hb-logo-monogram-emblem-style-with-crown-shape-design-template-free-vector.jpg" alt="logo" style={{width:"100%"}}/>
+    </div>
+    <div className="column">
+      <form action="/action_page.php">
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+        <label for="country">Country</label>
+        <select id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+        <label for="subject">Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style={{height:"170px"}}></textarea>
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
+  </div>
+</div>
+    </>
   )
 }
 
