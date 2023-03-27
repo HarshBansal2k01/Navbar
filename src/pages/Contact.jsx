58ee348d8cb3a2191647f4bddf2def351792e936
HarshBansal2k01 <ihbbro14@gmail.com>
2023-03-27 10:54:49 +0530

    pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,39 +1,98 @@
-import React from 'react'
-import "../style/contact.css"
+import React from "react";
+import "../style/contact.css";
 
 function Contact() {
   return (
     <>
-      <div className="container">
-  <div style={{textAlign:"center"}}>
-    <h2>Contact Us</h2>
-    <p>Swing by for a cup of coffee, or leave us a message:</p>
-  </div>
-  <div className="row">
-    <div className="column">
-      <img src="https://static.vecteezy.com/system/resources/previews/004/283/797/original/hb-logo-monogram-emblem-style-with-crown-shape-design-template-free-vector.jpg" alt="logo" style={{width:"100%"}}/>
-    </div>
-    <div className="column">
-      <form action="/action_page.php">
-        <label for="fname">First Name</label>
-        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-        <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-        <label for="country">Country</label>
-        <select id="country" name="country">
-          <option value="australia">Australia</option>
-          <option value="canada">Canada</option>
-          <option value="usa">USA</option>
-        </select>
-        <label for="subject">Subject</label>
-        <textarea id="subject" name="subject" placeholder="Write something.." style={{height:"170px"}}></textarea>
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  </div>
-</div>
+      <div
+        className="w3-container w3-content w3-padding-64"
+        style={{ maxWidth: "800px" }}
+        id="contact"
+      >
+        <h2 className="w3-wide w3-center">CONTACT</h2>
+        <p className="w3-opacity w3-center">
+          <i>Fan? Drop a note!</i>
+        </p>
+        <div className="w3-row w3-padding-32">
+          <div className="w3-col m6 w3-large w3-margin-bottom">
+            <i className="fa fa-map-marker" style={{ width: "30px" }}></i>{" "}
+            Chicago, US
+            <br />
+            <i className="fa fa-phone" style={{ width: "30px" }}></i> Phone: +00
+            151515
+            <br />
+            <i className="fa fa-envelope" style={{ width: "30px" }}>
+              {" "}
+            </i>{" "}
+            Email: mail@mail.com
+            <br />
+          </div>
+          <div className="w3-col m6">
+            <form action="/action_page.php" target="_blank">
+              <div
+                className="w3-row-padding"
+                style={{ margin: "0 -16px 8px -16px" }}
+              >
+                <div className="w3-half">
+                  <input
+                    className="w3-input w3-border"
+                    type="text"
+                    placeholder="Name"
+                    required
+                    name="Name"
+                  />
+                </div>
+                <div className="w3-half">
+                  <input
+                    className="w3-input w3-border"
+                    type="text"
+                    placeholder="Email"
+                    required
+                    name="Email"
+                  />
+                </div>
+              </div>
+              <input
+                className="w3-input w3-border"
+                type="text"
+                placeholder="Message"
+                required
+                name="Message"
+              />
+              <button
+                className="w3-button w3-black w3-section w3-right"
+                type="submit"
+              >
+                SEND
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <img
+        src="https://www.w3schools.com/w3images/map.jpg"
+        className="w3-image w3-greyscale-min"
+        alt=""
+        style={{ width: "100%" }}
+      />
+
+      <footer className="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+        <i className="fa fa-facebook-official w3-hover-opacity"></i>
+        <i className="fa fa-instagram w3-hover-opacity"></i>
+        <i className="fa fa-snapchat w3-hover-opacity"></i>
+        <i className="fa fa-pinterest-p w3-hover-opacity"></i>
+        <i className="fa fa-twitter w3-hover-opacity"></i>
+        <i className="fa fa-linkedin w3-hover-opacity"></i>
+        <p className="w3-medium">
+          Powered by{" "}
+          <a href="/" target="_blank">
+            Harsh Bansal
+          </a>
+        </p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,54 +3,41 @@ import React from 'react'
 function Home() {
   return (
     <>
-      {/* <div className="w3-content" style={{maxWidth:"2000px",marginTop:"46px"}}>
+      <div className="w3-content" style={{maxWidth:"2000px",marginTop:"46px"}}>
 
 <div className="mySlides w3-display-container w3-center">
-  <img src="/w3images/la.jpg" style="width:100%"/>
+  <img src="https://images.unsplash.com/photo-1670349148055-e11a0b3be242?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt='' style={{width:"100%"}}/>
   <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
     <h3>Los Angeles</h3>
     <p><b>We had the best time playing at Venice Beach!</b></p>   
   </div>
 </div>
 <div className="mySlides w3-display-container w3-center">
-  <img src="/w3images/ny.jpg" style="width:100%"/>
+<img src="https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1216&q=80" alt="" />
   <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
     <h3>New York</h3>
     <p><b>The atmosphere in New York is lorem ipsum.</b></p>    
   </div>
 </div>
 <div className="mySlides w3-display-container w3-center">
-  <img src="/w3images/chicago.jpg" style="width:100%"/>
+  <img src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1244&q=80" alt='' style={{width:"100%"}}/>
   <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
     <h3>Chicago</h3>
     <p><b>Thank you, Chicago - A night we won't forget.</b></p>    
   </div>
 </div>
 
-<div className="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+<div className="w3-container w3-content w3-center w3-padding-64" style={{maxWidth:"800px"}} id="band">
   <h2 className="w3-wide">THE BAND</h2>
   <p className="w3-opacity"><i>We love music</i></p>
   <p className="w3-justify">We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
     adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-  <div className="w3-row w3-padding-32">
-    <div className="w3-third">
-      <p>Name</p>
-      <img src="/w3images/bandmember.jpg" className="w3-round w3-margin-bottom" alt="Random Name" style="width:60%"/>
-    </div>
-    <div className="w3-third">
-      <p>Name</p>
-      <img src="/w3images/bandmember.jpg" className="w3-round w3-margin-bottom" alt="Random Name" style="width:60%"/>
-    </div>
-    <div className="w3-third">
-      <p>Name</p>
-      <img src="/w3images/bandmember.jpg" className="w3-round" alt="Random Name" style="width:60%"/>
-    </div>
-  </div>
+
 </div>
 
 <div className="w3-black" id="tour">
-  <div className="w3-container w3-content w3-padding-64" style="max-width:800px">
+  <div className="w3-container w3-content w3-padding-64" style={{maxWidth:"800px"}}>
     <h2 className="w3-wide w3-center">TOUR DATES</h2>
     <p className="w3-opacity w3-center"><i>Remember to book your tickets!</i></p><br/>
 
@@ -60,9 +47,8 @@ function Home() {
       <li className="w3-padding">November <span className="w3-badge w3-right w3-margin-right">3</span></li>
     </ul>
 
-    <div className="w3-row-padding w3-padding-32" style="margin:0 -16px">
+    <div className="w3-row-padding w3-padding-32" style={{margin:"0 -16px"}}>
       <div className="w3-third w3-margin-bottom">
-        <img src="/w3images/newyork.jpg" alt="New York" style="width:100%" className="w3-hover-opacity"/>
         <div className="w3-container w3-white">
           <p><b>New York</b></p>
           <p className="w3-opacity">Fri 27 Nov 2016</p>
@@ -71,7 +57,6 @@ function Home() {
         </div>
       </div>
       <div className="w3-third w3-margin-bottom">
-        <img src="/w3images/paris.jpg" alt="Paris" style="width:100%" className="w3-hover-opacity"/>
         <div className="w3-container w3-white">
           <p><b>Paris</b></p>
           <p className="w3-opacity">Sat 28 Nov 2016</p>
@@ -80,7 +65,6 @@ function Home() {
         </div>
       </div>
       <div className="w3-third w3-margin-bottom">
-        <img src="/w3images/sanfran.jpg" alt="San Francisco" style="width:100%" className="w3-hover-opacity"/>
         <div className="w3-container w3-white">
           <p><b>San Francisco</b></p>
           <p className="w3-opacity">Sun 29 Nov 2016</p>
@@ -106,39 +90,15 @@ function Home() {
       <input className="w3-input w3-border" type="text" placeholder="Enter email"/>
       <button className="w3-button w3-block w3-teal w3-padding-16 w3-section w3-right">PAY <i className="fa fa-check"></i></button>
       <button className="w3-button w3-red w3-section" onclick="document.getElementById('ticketModal').style.display='none'">Close <i className="fa fa-remove"></i></button>
-      <p className="w3-right">Need <a href="#" className="w3-text-blue">help?</a></p>
-    </div>
-  </div>
-</div>
-
-<div className="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
-  <h2 className="w3-wide w3-center">CONTACT</h2>
-  <p className="w3-opacity w3-center"><i>Fan? Drop a note!</i></p>
-  <div className="w3-row w3-padding-32">
-    <div className="w3-col m6 w3-large w3-margin-bottom">
-      <i className="fa fa-map-marker" style="width:30px"></i> Chicago, US<br/>
-      <i className="fa fa-phone" style="width:30px"></i> Phone: +00 151515<br/>
-      <i className="fa fa-envelope" style="width:30px"> </i> Email: mail@mail.com<br/>
-    </div>
-    <div className="w3-col m6">
-      <form action="/action_page.php" target="_blank">
-        <div className="w3-row-padding" style="margin:0 -16px 8px -16px">
-          <div className="w3-half">
-            <input className="w3-input w3-border" type="text" placeholder="Name" required name="Name"/>
-          </div>
-          <div className="w3-half">
-            <input className="w3-input w3-border" type="text" placeholder="Email" required name="Email"/>
-          </div>
-        </div>
-        <input className="w3-input w3-border" type="text" placeholder="Message" required name="Message"/>
-        <button className="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
-      </form>
+      <p className="w3-right">Need <a href="/" className="w3-text-blue">help?</a></p>
     </div>
   </div>
 </div>
 
 
-</div> */}
+
+
+</div>
 
     </>
   )
